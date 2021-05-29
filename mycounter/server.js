10d28b41c0577e72c4c079eec8f0d4bc8c8b50ae
skyhilam <skyhilam@gmail.com>
2021-05-29 13:08:57 +0800

    save

--- mycounter/server.js
+++ mycounter/server.js
@@ -1,8 +1,17 @@
 const app = require('express')();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 const http = require('http').Server(app);
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const io = require('socket.io')(http);
+
+const fs = require('fs');
+const htmltopdf = require('html-pdf');
+
 
 var formdata = {
   items: [],
@@ -15,9 +24,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+
 app.get('/', (req, resp) => {
-  resp.sendFile(__dirname + '/index.html');
+  resp.sendFile(__dirname + '/html/index.html');
 });
+
+app.post('/receipt', (req, resp) => {
+  const data = req.body;
+  const html = fs.readFileSync(__dirname + '/html/receipt.html', 'utf-8');
+  htmltopdf.create(html, {
+    format: 'A4',
+    "quality": "100",   
+    "phantomPath": "./node_modules/phantomjs/bin/phantomjs",
+  }).toFile('./pdf/test.pdf', (err, _resp) => {
+    if (err) return console.log(err);
+    console.log(_resp);
+    resp.sendStatus(200);
+  })
+
+  // console.log(html);
+  
+})
 
 app.post('/', (req, resp) => {
   formdata = req.body;
@@ -30,5 +57,5 @@ io.on('connection', (socket) => {
 })
 
 http.listen(3333, () => {
-  console.log('started server');
+  console.log('http://localhost:3333');
 });
